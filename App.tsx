0e10e12823ca09f7bcbe4682f5aa2a026758f7ca
Mads Røskar <madshvero@gmail.com>
2022-02-07 20:58:58 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Text } from "react-native";
+import { StreamChat } from "stream-chat";
+import { OverlayProvider, Chat, ChannelList } from "stream-chat-expo";
+
+const streamApi = "YOUR_API_KEY";
+
+const client = StreamChat.getInstance(streamApi);
 
 export default function App() {
+  const [selectedChannel, setSelectedChannel] = useState(null);
+  const [isReady, setIsReady] = useState(false);
+
+  const onChannelPressed = (channel: any) => {
+    setSelectedChannel(channel);
+  };
+
+  useEffect(() => {
+    const connectUser = async () => {
+      await client.connectUser(
+        {
+          id: "user",
+          name: "User Name",
+          image: "https://i.imgur.com/fR9Jz14.png",
+        },
+        client.devToken("user")
+      );
+
+      const channel = client.channel("messaging", "test_channel", {
+        name: "Test Channel",
+      });
+
+      await channel.watch();
+      setIsReady(true);
+    };
+
+    connectUser();
+
+    return () => {
+      client.disconnectUser();
+    };
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <OverlayProvider>
+        <Chat client={client}>
+          {selectedChannel ? (
+            <Text>Channel Page</Text>
+          ) : (
+            <ChannelList onSelect={onChannelPressed} />
+          )}
+        </Chat>
+      </OverlayProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,9 @@
+# Stream Chat Expo with a ChannelList
+
+Repro with a working channel list that can select a channel.
+
+## Setup
+
+1. Change the `APP_ID` defined in [`App.tsx`](./App.tsx)
+2. Install dependencies (`yarn install # or npm install`)
+3. Run with `expo start`
